fd1dc9906b84dc27ea757a6384539ff0625e82fa
ishutov <sidjimmy18@gmail.com>
2015-01-19 14:49:31 +0400

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,17 @@
+#include "main.h"
+
+/* Экран */
+SDL_Surface *screen = NULL;		
+
 int main(int argc, char** argv)
 {
 	return 0;
+}
+
+/* Создание окна */
+void makewindow()
+{	
+    screen = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE|SDL_DOUBLEBUF|SDL_FULLSCREEN);    
+    SDL_WM_SetCaption ("iTetris", NULL);
+    SDL_Flip(screen);
 }
