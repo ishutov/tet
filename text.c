6a0fc9df5a0b3a8f3d56a4b54d4f14e99504c535
ishutov <sidjimmy18@gmail.com>
2015-01-21 15:09:51 +0400

    Create text.c

---
+++ text.c
@@ -0,0 +1,17 @@
+#include "text.h"
+
+/* Отрисовка любого текста */
+void write(SDL_Surface *screen, const char* textOnScreen, SDL_Color colorText, int posX, int posY)
+{    
+  SDL_Surface *text = NULL;
+	SDL_Rect textPosition; 
+  TTF_Font *font = TTF_OpenFont("data/font.ttf", 22);
+		   
+  text = TTF_RenderText_Solid(font, textOnScreen, colorText);
+  textPosition.x = posX;
+  textPosition.y = posY;
+  textPosition.h = text->h;
+  textPosition.w = text->w;
+  SDL_FillRect(screen, &textPosition, SDL_MapRGB(screen->format, 0, 0, 0));
+  SDL_BlitSurface(text, NULL, screen, &textPosition);
+}
