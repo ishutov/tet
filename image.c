6b21ff07337cf2b542a8ff88d57f7be73e600505
ishutov <sidjimmy18@gmail.com>
2015-01-24 15:00:03 +0400

    Update image.c

--- image.c
+++ image.c
@@ -6,6 +6,6 @@ SDL_Surface *loadImage(int number)
   SDL_Surface *image = NULL;	    
   char *filename = (char*)malloc(sizeof(char*) * (strlen("data/")+3+strlen(".png")));
   sprintf(filename, "data/%d.png", number);
-	image = IMG_Load(filename);
-	return image;    
+  image = IMG_Load(filename);
+  return image;    
 }
