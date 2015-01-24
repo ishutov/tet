1b4b6417f6ec0c085b726831660ea838883ebffb
ishutov <sidjimmy18@gmail.com>
2015-01-24 16:07:35 +0400

    Update text.c

--- text.c
+++ text.c
@@ -4,7 +4,7 @@
 void write(SDL_Surface *screen, const char* textOnScreen, SDL_Color colorText, int posX, int posY)
 {    
   SDL_Surface *text = NULL;
-	SDL_Rect textPosition; 
+  SDL_Rect textPosition; 
   TTF_Font *font = TTF_OpenFont("data/font.ttf", 22);
 		   
   text = TTF_RenderText_Solid(font, textOnScreen, colorText);
